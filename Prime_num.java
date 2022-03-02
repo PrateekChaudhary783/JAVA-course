4fb4e394ce4fab046606eefc2951a0db0479fed3
PrateekChaudhary783 <98892241+PrateekChaudhary783@users.noreply.github.com>
2022-03-02 16:03:14 +0530

    Prime

---
+++ Prime_num.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Prime_num {
+
+	public static void main(String[] args) {
+		Scanner obj=new Scanner(System.in);
+		System.out.println("Please insert any number :");
+		    int num = obj.nextInt();
+		    boolean flag = false;
+		    for (int i = 2; i <= num / 2; ++i) {
+		      if (num % i == 0) {
+		        flag = true;
+		        break;
+		      }
+		    }
+
+		    if (!flag)
+		      System.out.println(num + " is a prime number.");
+		    else
+		      System.out.println(num + " is not a prime number.");
+		  }
+		}
+
+
