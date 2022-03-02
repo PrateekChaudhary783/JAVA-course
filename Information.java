1d60978e3e3da0a2398a802481511559c1f5aa55
PrateekChaudhary783 <98892241+PrateekChaudhary783@users.noreply.github.com>
2022-03-02 15:47:04 +0530

    Add files via upload

---
+++ Information.java
@@ -0,0 +1,8 @@
+
+public class Information {
+	String one="Robert \t  1994 \t \t  64C- WallsStreat";
+	String two="Sam \t  2000 \t \t  68D- WallsStreat";
+	String three="John \t  1999 \t \t  26B- WallsStreat";
+	
+
+}
---
+++ Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Information c= new Information();
+		System.out.println("Name \t Year of joining \tAddress");
+		System.out.println(c.one);
+		System.out.println(c.two);
+		System.out.println(c.three);
+	}
+
+}
